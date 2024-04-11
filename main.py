7e9e7acb672809d556dd610f5a49183a0e6fd1ee
Phanurat <paunavt5@gmail.com>
2024-04-11 10:16:07 +0700

    SetUp Kivy Python

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello, Kivy!')
+
+if __name__ == '__main__':
+    MyApp().run()
